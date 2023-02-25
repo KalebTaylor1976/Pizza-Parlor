d2af0e0972468e8ef9ca667addc5c52e8100c2e6
Kaleb Taylor <kaleb.taylor1976@gmail.com>
2023-02-25 15:33:44 -0800

    add Pizza caculate cost

--- scripts.js
+++ scripts.js
@@ -9,3 +9,18 @@ Pizza.prototype.calculateCost = function() {
     case "small":
       cost = 8;
       break;
+      case "medium":
+      cost = 10;
+      break;
+    case "large":
+      cost = 12;
+      break;
+    default:
+      break;
+  }
+  if (this.toppings.includes("cheese")) {
+    cost += 1;
+  }
+  if (this.toppings.includes("pepperoni")) {
+    cost += 2;
+  }
